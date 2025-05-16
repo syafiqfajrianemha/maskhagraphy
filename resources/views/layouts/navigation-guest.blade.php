f56131be04658de3f3b26db6ac9ae14a6dcb6ad2
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2025-05-16 17:00:05 +0700

    fix

--- resources/views/layouts/navigation-guest.blade.php
+++ resources/views/layouts/navigation-guest.blade.php
@@ -143,35 +143,64 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        {{-- <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('home.index')" :active="request()->routeIs('home.index')">
+                {{ __('Home') }}
             </x-responsive-nav-link>
-        </div> --}}
+            <x-responsive-nav-link :href="route('booking.index')" :active="request()->routeIs('booking.index')">
+                {{ __('Booking') }}
+            </x-responsive-nav-link>
+            @if (Route::has('login'))
+                @auth
+                    <x-responsive-nav-link :href="route('collections.index')" :active="request()->routeIs('collections.index')">
+                        {{ __('Collections') }}
+                    </x-responsive-nav-link>
+                @endauth
+            @endif
+            <a
+                href="{{ route('cart.index') }}"
+                class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-white/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-black dark:hover:text-black/80 dark:focus-visible:ring-black relative mr-5"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                </svg>
+                <span class="text-xs absolute bottom-5 right-1 text-black">{{ $cartCount }}</span>
+            </a>
+        </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
-            {{-- <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div> --}}
+            @if (Route::has('login'))
+                @auth
+                    <div class="px-4">
+                        <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                        <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    </div>
 
-            <div class="mt-3 space-y-1">
-                {{-- <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link> --}}
+                    <div class="mt-3 space-y-1">
+                        <x-responsive-nav-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-responsive-nav-link>
 
-                <!-- Authentication -->
-                {{-- <form method="POST" action="{{ route('logout') }}">
-                    @csrf
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
 
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form> --}}
-            </div>
+                            <x-responsive-nav-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-responsive-nav-link>
+                        </form>
+                    </div>
+                    @else
+                    <a
+                        href="{{ route('login') }}"
+                        class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-white/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-black dark:hover:text-black/80 dark:focus-visible:ring-black"
+                    >
+                        Log in
+                    </a>
+                @endauth
+            @endif
         </div>
     </div>
 </nav>
